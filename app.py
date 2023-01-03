9d67e117739fe20c1dfe678ecb1c3996c96839b7
Taylor D. Edmiston <tedmiston@gmail.com>
2023-01-03 16:39:42 -0500

    Add exception handling

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ logging.basicConfig(
 
 
 def fetch_page():
+    logging.info('Fetching puzzle')
+
     url = "https://www.nytimes.com/puzzles/spelling-bee"
     response = requests.get(url)
 
@@ -26,35 +28,35 @@ def fetch_page():
 
 
 def extract_game_data(response):
+    logging.info('Extracting game data')
+
     if not response.ok:
-        logging.error("HTTP response code was not successful")
-        exit(1)
+        raise Exception('HTTP response code was not successful')
 
     soup = BeautifulSoup(response.content, "html.parser")
     game_data_script = soup.find("script", string=re.compile("^window.gameData.*"))
 
     if not game_data_script:
-        logging.error("Game data script was not found")
-        exit(1)
+        raise Exception("Game data script was not found")
 
     return game_data_script
 
 
 def parse_game_data(game_data_script):
+    logging.info('Parsing game data')
+
     yesterday_start_index = game_data_script.text.find('"yesterday"')
     yesterday_script_text = game_data_script.text[yesterday_start_index:]
     yesterday_end_index = yesterday_script_text.find("}") + 1
     yesterday_script_text = yesterday_script_text[:yesterday_end_index]
 
     if yesterday_start_index < 0 or yesterday_end_index < 0:
-        logging.error("Yesterday game data could not be parsed")
-        exit(1)
+        raise Exception("Yesterday game data could not be parsed")
 
     try:
         yesterday_dict = json.loads("{" + yesterday_script_text + "}")
     except json.decoder.JSONDecodeError:
-        logging.error('JSON decoding of yesterday game data failed')
-        exit(1)
+        raise Exception("JSON decoding of yesterday game data failed")
 
     pprint(yesterday_dict["yesterday"], sort_dicts=False)
 
@@ -62,6 +64,8 @@ def parse_game_data(game_data_script):
 
 
 def output_game_answers_data(yesterday_dict):
+    logging.info('Writing game data')
+
     yesterday_date = datetime.date.today() - datetime.timedelta(days=1)
     output = json.dumps(yesterday_dict["yesterday"], indent=2)
     with open(f"days/{yesterday_date}.json", "w") as fp:
