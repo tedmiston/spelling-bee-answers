40eb8abad8d5d472b575d2c39cbc28e7f3c840f0
Taylor D. Edmiston <tedmiston@gmail.com>
2023-01-24 21:38:44 -0500

    Add SBSolver scraper PoC

---
+++ spelling_bee_answers/tools/sbsolver_parser.py
@@ -0,0 +1,90 @@
+"""
+Scrape puzzle data from the SBSolver archive.
+
+Example:
+- https://www.sbsolver.com/archive/2018/05
+- https://www.sbsolver.com/s/1
+"""
+
+from dataclasses import dataclass
+from pprint import pprint
+from typing import List, Optional
+
+from bs4 import BeautifulSoup
+import requests
+
+# temporary data class model to be replaced with a pydantic model in prod
+@dataclass
+class Puzzle:
+    date: str
+    center_letter: str
+    outer_letters: List[str]
+    points: int
+    answers: List[str]
+    editor: Optional[str]
+    verified: bool
+
+
+def main():
+    # this puzzle id in the url *does not* correspond to the nytimes puzzle id
+    url = "https://www.sbsolver.com/s/1"
+    # url = "https://www.sbsolver.com/s/1721"
+    response = requests.get(url)
+
+    if not response.ok:
+        raise Exception("ut oh")
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    date = soup.find("span", attrs={"class": "bee-date"}).find("a").text
+    # todo: parse date string
+    # print(date)
+
+    all_letters = [
+        x["alt"]
+        for x in soup.find("div", attrs={"id": "alpha-inner"})
+        .find("div", attrs={"class": "bee-medium"})
+        .find("div", attrs={"class": "thinner-space-after"})
+        .find_all("img")
+    ]
+    # print(all_letters)
+
+    # extract center letter and outer letters
+    center_letter = None
+    outer_letters = []
+    for i in all_letters:
+        if i.startswith("center letter"):
+            center_letter = i.rsplit(" ")[-1].lower()
+        else:
+            outer_letters.append(i.lower())
+
+    points = int(
+        soup.find_all("span", attrs={"class": "bee-loud"})[-1].text.split(" ")[0]
+    )
+    # print(points)
+
+    answers_table = soup.find("table", attrs={"class": "bee-set"})
+    answers = [
+        x.text for x in answers_table.find_all("td", attrs={"class": "bee-hover"})
+    ]
+    answers = sorted([x.lower() for x in answers])
+    # pprint(answers)
+
+    editor = None  # in this context null means unknown
+
+    verified = False
+
+    puzzle = Puzzle(
+        date=date,
+        center_letter=center_letter,
+        outer_letters=outer_letters,
+        points=points,
+        answers=answers,
+        editor=editor,
+        verified=verified,
+    )
+    pprint(puzzle)
+
+
+if __name__ == "__main__":
+    main()
