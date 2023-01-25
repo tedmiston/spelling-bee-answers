7a1f64752f0d7186bdf6c25dff37de31296a4008
Taylor D. Edmiston <tedmiston@gmail.com>
2023-01-25 13:11:58 -0500

    Add Puzzle model

---
+++ spelling_bee_answers/models.py
@@ -0,0 +1,46 @@
+"""
+Models.
+"""
+
+import json
+from datetime import date
+# from pprint import pprint
+from typing import List, Optional
+
+# import pendulum
+from pydantic import BaseModel
+from rich import print
+
+
+class Puzzle(BaseModel):
+    date: date
+    center_letter: str
+    outer_letters: List[str]
+    points: Optional[int]
+    answers: List[str]
+    pangrams: List[str]
+    editor: Optional[str]
+    verified: bool
+
+
+def main():
+    filepath = "days/2023-01-01.json"
+    # filepath = "days/2023-01-23.json"
+    with open(filepath) as fp:
+        obj = json.load(fp)
+        # pprint(obj)
+        p = Puzzle(
+            date=obj["printDate"],
+            center_letter=obj["centerLetter"],
+            outer_letters=obj["outerLetters"],
+            points=None,  # todo: see if i can get this from somewhere
+            answers=obj["answers"],
+            pangrams=obj["pangrams"],
+            editor=obj["editor"],
+            verified=True,
+        )
+        print(p)
+
+
+if __name__ == "__main__":
+    main()
