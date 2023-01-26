a913b44be447f557c396a64416811d8ded085e36
Taylor D. Edmiston <tedmiston@gmail.com>
2023-01-25 19:05:19 -0500

    Drop trivial helper functions

--- spelling_bee_answers/stats.py
+++ spelling_bee_answers/stats.py
@@ -46,7 +46,7 @@ def determine_counts(words):
     return counts
 
 
-def _generate_words_table(counts, condition=lambda _: True):
+def generate_words_table(counts, condition=lambda _: True):
     headers = ["Word", "Count", "Definition"]
     rows = [
         (f"**{word}**", count, f"https://www.wordnik.com/words/{word}")
@@ -56,18 +56,6 @@ def _generate_words_table(counts, condition=lambda _: True):
     table = tabulate(rows, headers=headers, tablefmt="github")
     # print(table)
     return table
-
-
-def generate_all_words_table(counts):
-    return _generate_words_table(counts)
-
-
-def generate_multi_count_words_table(counts):
-    return _generate_words_table(counts, lambda count: count > 1)
-
-
-def generate_pangrams_table(counts):
-    return _generate_words_table(counts)
 
 
 def update_doc(filename, tag, table, word_count, label):
@@ -100,14 +88,14 @@ def main():
     update_doc(
         filename="Words.md",
         tag="generated multi table",
-        table=generate_multi_count_words_table(all_answers_counts),
+        table=generate_words_table(all_answers_counts, lambda count: count > 1),
         word_count=len([x for x in all_answers_counts.values() if x > 1]),
         label="words",
     )
     update_doc(
         filename="Words.md",
         tag="generated all table",
-        table=generate_all_words_table(all_answers_counts),
+        table=generate_words_table(all_answers_counts),
         word_count=len(all_answers_counts.keys()),
         label="words",
     )
@@ -118,7 +106,7 @@ def main():
     update_doc(
         filename="Pangrams.md",
         tag="generated table",
-        table=generate_pangrams_table(pangrams_counts),
+        table=generate_words_table(pangrams_counts),
         word_count=len(pangrams_counts.keys()),
         label="pangrams",
     )
