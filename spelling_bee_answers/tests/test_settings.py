db6a6197ec0a6e265381828f5c424a8e53a4d99e
Taylor D. Edmiston <tedmiston@gmail.com>
2023-01-25 19:08:22 -0500

    Add settings tests

---
+++ spelling_bee_answers/tests/test_settings.py
@@ -0,0 +1,40 @@
+"""
+Unit tests - settings.
+"""
+
+import logging
+
+from ..settings import Settings
+
+
+def test_settings_schema():
+    settings = Settings()
+
+    assert isinstance(settings.repo_root, str)
+
+    assert isinstance(settings.log_level, str)
+    log_levels = list(logging.getLevelNamesMapping().keys())
+    assert settings.log_level in log_levels
+
+    assert isinstance(settings.display_puzzle_output, bool)
+
+    assert isinstance(settings.display_generated_readme_output, bool)
+
+    assert isinstance(settings.display_generated_readme_table, bool)
+
+
+def test_settings_defaults():
+    settings = Settings()
+
+    assert settings.repo_root != ""
+
+    assert settings.log_level == "INFO"
+
+    assert settings.display_puzzle_output == True
+
+    assert settings.display_generated_readme_output == False
+
+    assert settings.display_generated_readme_table == True
+
+
+# todo: test_settings_env_var_overrides
